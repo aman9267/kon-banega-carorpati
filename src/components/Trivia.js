e4ae55cf0a8ff37f6d8f6d901dec07eb500298d2
Mohd Aman <mohdaman@Mohds-MacBook-Pro.local>
2024-01-24 12:44:27 +0530

    Update KBC Game

--- src/components/Trivia.js
+++ src/components/Trivia.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
+import "./trivia.css";
 
-const Trivia = ({ data, questionActive, setTimeStop, setQuestionActive }) => {
+const Trivia = ({ data, questionActive, setQuestionActive }) => {
   const [question, setQuestion] = useState(1);
   const [selectedAnswer, setSelectedAnswer] = useState(null);
   const [className, setClassName] = useState("null");
@@ -8,46 +9,58 @@ const Trivia = ({ data, questionActive, setTimeStop, setQuestionActive }) => {
     setQuestion(data[questionActive - 1]);
   }, [data, questionActive, selectedAnswer]);
 
+  const [wrong, setWrong] = useState(true);
+
   const delay = (duration, callback) => {
     setTimeout(() => {
-      callback()
+      callback();
     }, duration);
   };
 
-  console.log(selectedAnswer);
   const handleClick = (answer) => {
     setSelectedAnswer(answer);
     setClassName("answer active");
 
-    delay(3000, ()=>{
+    delay(3000, () => {
       setClassName(answer.correct === true ? "answer correct" : "answer wrong");
-    })
+    });
     delay(7000, () => {
       if (answer.correct) {
         setQuestionActive((pre) => pre + 1);
-        // setQuestionActive(null);
       } else {
-        setTimeStop(true);
+        setWrong(false);
       }
     });
   };
   return (
-    <div className="trivia">
-      <div className="question">{question.question}</div>
-      <div className="answers">
-        {question?.answers?.map((answer) => {
-          return (
-            <div
-              key={answer?.id}
-              onClick={() => handleClick(answer)}
-              className={selectedAnswer === answer ? className : "answer"}
-            >
-              {answer?.text}
-            </div>
-          );
-        })}
-      </div>
-    </div>
+    <>
+      {wrong ? (
+        <div className="trivia">
+          <div className="question">{question.question}</div>
+          <div className="answers">
+            {question?.answers?.map((answer) => {
+              return (
+                <div
+                  key={answer?.id}
+                  onClick={() => handleClick(answer)}
+                  className={selectedAnswer === answer ? className : "answer"}
+                >
+                  {answer?.text}
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      ) : (
+        <div className="false__answer">
+          <div className="false__model">
+            <h3>Opp! Wrong answer you winning price</h3>
+            <h2>₹{question.price}</h2>
+            <a href="/">Restart The Game</a>
+          </div>
+        </div>
+      )}
+    </>
   );
 };
 
